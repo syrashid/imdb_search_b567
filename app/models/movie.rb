58063b5a84d6f7c2ef4e3dad47ea10d6b98da99c
Sy Rashid <sy.rashid.11@gmail.com>
2021-03-04 02:30:53 -0500

    FEAT: Finished pgsearch tutorial

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ApplicationRecord
   belongs_to :director
+  # include PgSearch::Model
+  # pg_search_scope :search_by_title_and_synopsis,
+  #   against: [ :title, :synopsis ],
+  #   associated_against: {
+  #     director: [ :first_name, :last_name ]
+  #   },
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
+  include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
 end
