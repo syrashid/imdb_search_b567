9f72d06f2f929bd5b4f17d74d38bac5d168f9c08
Sy Rashid <sy.rashid.11@gmail.com>
2021-03-04 02:08:27 -0500

    FEAT: Set up base search with AR and sql

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:query].present?
+      # Do some sort of search
+      sql_query = "\
+        movies.title @@ :query \
+        OR movies.synopsis @@ :query \
+        OR directors.first_name @@ :query \
+        OR directors.last_name @@ :query \
+      "
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
+
   end
 end
